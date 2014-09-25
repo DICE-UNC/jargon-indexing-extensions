439531806aa3dde480f89f081d111a62585e8d3c
mconway <michael_conway@unc.edu>
2014-09-25 08:56:08 -0400

    #6 adding file logger

--- file-action-logging-indexer/src/main/java/org/irods/jargon/indexing/fileactlog/FileActionLoggingIndexer.java
+++ file-action-logging-indexer/src/main/java/org/irods/jargon/indexing/fileactlog/FileActionLoggingIndexer.java
@@ -4,6 +4,7 @@
 package org.irods.jargon.indexing.fileactlog;
 
 import org.irods.jargon.indexing.wrapper.IndexerWrapper;
+import org.irods.jargon.indexing.wrapper.event.FileEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -37,6 +38,32 @@ public class FileActionLoggingIndexer extends IndexerWrapper {
 	protected void onShutdown() {
 		log.info("<<<<<<<<<< shutdown of  indexer");
 
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.irods.jargon.indexing.wrapper.IndexerWrapper#onFileAdd(org.irods.
+	 * jargon.indexing.wrapper.event.FileEvent)
+	 */
+	@Override
+	protected void onFileAdd(FileEvent fileEvent) {
+		log.info(">>>>> File Add");
+		log.info("fileEvent:{}", fileEvent);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.irods.jargon.indexing.wrapper.IndexerWrapper#onFileDelete(org.irods
+	 * .jargon.indexing.wrapper.event.FileEvent)
+	 */
+	@Override
+	protected void onFileDelete(FileEvent fileEvent) {
+		log.info(">>>>> File Delete");
+		log.info("fileEvent:{}", fileEvent);
 	}
 
 }
