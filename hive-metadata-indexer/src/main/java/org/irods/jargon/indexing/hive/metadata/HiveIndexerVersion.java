3b7c5fa5c3784ab57434c93ab331fbd632bda17c
mconway <michael_conway@unc.edu>
2014-09-29 10:38:18 -0400

    #7 move out hive indexer

--- hive-metadata-indexer/src/main/java/org/irods/jargon/indexing/hive/metadata/HiveIndexerVersion.java
+++
@@ -1,5 +0,0 @@
-package org.irods.jargon.indexing.hive.metadata;
-public final class HiveIndexerVersion {
- public static String VERSION="1.0.0-SNAPSHOT";
- public static String BUILD_TIME="20140925-1030";
-}
